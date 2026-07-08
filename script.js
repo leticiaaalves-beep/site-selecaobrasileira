d710466f28f873561c1e43b01b3c18ae86833a3f
leticia <leticia@gmail.com>
2026-07-08 09:24:14 -0300

    site finalizado

--- script.js
+++ script.js
@@ -3,5 +3,22 @@ function aumentarimagem(){
     foto.classList.toggle("aumentada");
     }
     
+function aumentarimagem2(){
+     const foto = document.getElementById("ft_selecao2");
+     foto.classList.toggle("aumentada");
+}
 
+function aumentarimagem3(){
+     const foto = document.getElementById("ft_selecao3");
+     foto.classList.toggle("aumentada")
+}
 
+function aumentarimagem4(){
+     const foto = document.getElementById("ft_selecao4");
+     foto.classList.toggle("aumentada");
+}
+
+function aumentarimagem5(){
+     const foto = document.getElementById("ft_selecao5");
+     foto.classList.toggle("aumentada");
+}
